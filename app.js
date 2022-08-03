e953d966a8c645b4217410a43303c7dcbd4a52a3
Julian Ortiz <julianortiz@MacBook-Air-de-Julian.local>
2022-08-02 23:21:40 -0300

    incorporamos la funcion divicion

--- app.js
+++ app.js
@@ -1,9 +1,13 @@
 
-
+//funcion dividir
+function dividir (n1,n2){
+    return n1 / n2
+}
+//le pedimos el nombre al usuario por teclado
 let nombre_usuario =prompt("Ingrese su nombre");
 alert(`${nombre_usuario} vienvenido a nuestro divisor automatico\nPodra realizar 4 opraciones`);
 
-for(let i=1; i <=3;i++){
+for(let i=1; i <=4;i++){
     
 let numeroUno = Number(prompt("Ingrese un numero a dividir \n del 1 al 10\n",));
 let numeroDos = Number(prompt("Ingrese  segundo numeroque desea dividir"));
@@ -12,11 +16,11 @@ let numeroDos = Number(prompt("Ingrese  segundo numeroque desea dividir"));
         alert(`No se puede dividir por 0`)
         break;  
     }else
-    alert(`Operacion Nro:${i}\n${numeroUno} / ${numeroDos} = ${numeroUno / numeroDos} `)
+    alert(`Operacion Nro:${i}\n${numeroUno} / ${numeroDos} = ${dividir(numeroUno,numeroDos)}`)
     
     
 } 
-alert(`${nombre_usuario},Gracias por utilizar nuestro divisor`) 
+alert(`${nombre_usuario},Gracias por utilizar nuestro divisor`);
 
 
 
