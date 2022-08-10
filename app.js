508479aa6a4f0b4515d7ff7dd7b26ace26f928f6
Julian Ortiz <julianortiz@MacBook-Air-de-Julian.local>
2022-08-10 13:50:33 -0300

    Incluimos Array de productos

--- app.js
+++ app.js
@@ -1,4 +1,4 @@
-
+/* 
 //Funcion Dividir
 function dividir (n1,n2){
     return n1 / n2
@@ -11,7 +11,6 @@ for(let i=1; i <=4;i++){
     
 let numeroUno = Number(prompt("Ingrese el primer numero a dividir "));
 let numeroDos = Number(prompt("Ingrese  segundo numero que desea dividir"));
-     
     if(numeroDos == 0 || numeroDos ==0){
         alert(`No se puede dividir por 0`)
         break;  
@@ -21,41 +20,54 @@ let numeroDos = Number(prompt("Ingrese  segundo numero que desea dividir"));
     
 } 
 alert(`${nombre_usuario},Gracias por utilizar nuestro divisor`);
-
+ */
 
 //Cargando productos DOBAL MUEBLES INDUSTRIALES
 
-/* 
-Mesa ratona elevable
-Mesa de arrime
-Estantería con cajón
-Mesa paraíso
-Rack estilo industrial
-Mesa ratona elevadle / con espacio de guardado */
-
-/* 
 class Productos{
-    constructor(codigo,nombre, medidas,precio){
+    constructor(codigo,nombre,color,madera, medidas,img,precio,){
         this.codigo=codigo;
-        this.nombre=nombre
+        this.nombre=nombre;
+        this.color=color;
+        this.madera=madera;
         this.medidas=medidas;
         this.precio=precio;
         
+        
     }
 
-    sumaIva(){return this.precio*1.21}
 }
 
-const prod1 = new Productos (01,"Mesa ratona elevadle","",50000);
-const prod2 = new Productos (02,"Mesa de arrime","",45000);
-const prod3 = new Productos (03,"Estantería con cajón","",150000);
-const prod4 = new Productos (04,"Mesa paraíso","",20000);
-const prod5 = new Productos (05,"Rack estilo industrial","",40000);
-const prod6 = new Productos (06,"Mesa ratona elevadle","",560000);
-
-console.log(prod2.nombre)
-console.log(prod1.sumaIva(prod1))
-console.log(Productos.length)
-*/
+let myArray = []
 
 
+
+let prod1 = new Productos (01,"MESA DE LUZ","NEGRO/BLANCO","ENCHAPADA EN PARAISO","50X30X35",49200);
+myArray.push(prod1)
+let prod2 = new Productos (02,"MESA RATONA ELEVABLE","NEGRO/BLANCO","ENCHAPADA EN PARAISO","85X50X45",51200);
+myArray.push(prod2)
+let prod3 = new Productos (03,"RACK TV","NEGRO/BLANCO","MACIZA EN LAUREL HIDROLAQUEADA","140X40X60",148200);
+myArray.push(prod3)
+let prod4 = new Productos (04,"BARRA EDSAYUNADORA","NEGRO/BLANCO","MACIZA DE PARAIZO","120X40X95",103200);
+myArray.push(prod4)
+let prod5 = new Productos (05,"MESA COMEDOR","NEGRO/BLANCO","MACIZA DE PARAIZO HIDROLAQUEADO","140X80X75",124200);
+myArray.push(prod5)
+let prod6 = new Productos (06,"BARRA MOVIL","NEGRO","MACIZA GUAYUBIRA HIDROLAQUEADA","110X45X95",145200); 
+myArray.push(prod6)
+
+
+myArray.forEach(product =>{
+    console.log(product)
+});
+
+
+/* for (let productNumber = 0; productNumber < 10; productNumber ++) {
+    const prod = new Productos(productNumber, `Producto ${productNumber}`, "", 1000 * (productNumber + 1));
+    myArray.push(prod);
+}
+
+ console.log(myArray)
+
+myArray.forEach(product => {
+    console.log(product.nombre)
+}); */
