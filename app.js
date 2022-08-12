1f2f39f3d080ea31f5aa4c7d772d37bc1c19446c
Julian Ortiz <julianortiz@MacBook-Air-de-Julian.local>
2022-08-12 00:48:28 -0300

    aniadimos metodos

--- app.js
+++ app.js
@@ -1,4 +1,4 @@
-/* 
+
 //Funcion Dividir
 function dividir (n1,n2){
     return n1 / n2
@@ -20,12 +20,12 @@ let numeroDos = Number(prompt("Ingrese  segundo numero que desea dividir"));
     
 } 
 alert(`${nombre_usuario},Gracias por utilizar nuestro divisor`);
- */
+
 
 //Cargando productos DOBAL MUEBLES INDUSTRIALES
 
-class Productos{
-    constructor(codigo,nombre,color,madera, medidas,img,precio,){
+class Producto{
+    constructor(codigo,nombre,color,madera, medidas,precio,){
         this.codigo=codigo;
         this.nombre=nombre;
         this.color=color;
@@ -42,23 +42,48 @@ let myArray = []
 
 
 
-let prod1 = new Productos (01,"MESA DE LUZ","NEGRO/BLANCO","ENCHAPADA EN PARAISO","50X30X35",49200);
+let prod1 = new Producto (01,"MESA DE LUZ","NEGRO/BLANCO","ENCHAPADA EN PARAISO","50X30X35",49200);
 myArray.push(prod1)
-let prod2 = new Productos (02,"MESA RATONA ELEVABLE","NEGRO/BLANCO","ENCHAPADA EN PARAISO","85X50X45",51200);
+let prod2 = new Producto (02,"MESA RATONA ELEVABLE","NEGRO/BLANCO","ENCHAPADA EN PARAISO","85X50X45",51200);
 myArray.push(prod2)
-let prod3 = new Productos (03,"RACK TV","NEGRO/BLANCO","MACIZA EN LAUREL HIDROLAQUEADA","140X40X60",148200);
+let prod3 = new Producto (03,"RACK TV","NEGRO/BLANCO","MACIZA EN LAUREL HIDROLAQUEADA","140X40X60",148200);
 myArray.push(prod3)
-let prod4 = new Productos (04,"BARRA EDSAYUNADORA","NEGRO/BLANCO","MACIZA DE PARAIZO","120X40X95",103200);
+let prod4 = new Producto (04,"BARRA EDSAYUNADORA","NEGRO/BLANCO","MACIZA DE PARAIZO","120X40X95",103200);
 myArray.push(prod4)
-let prod5 = new Productos (05,"MESA COMEDOR","NEGRO/BLANCO","MACIZA DE PARAIZO HIDROLAQUEADO","140X80X75",124200);
+let prod5 = new Producto (05,"MESA COMEDOR","NEGRO/BLANCO","MACIZA DE PARAIZO HIDROLAQUEADO","140X80X75",124200);
 myArray.push(prod5)
-let prod6 = new Productos (06,"BARRA MOVIL","NEGRO","MACIZA GUAYUBIRA HIDROLAQUEADA","110X45X95",145200); 
+let prod6 = new Producto (06,"BARRA MOVIL","NEGRO","MACIZA GUAYUBIRA HIDROLAQUEADA","110X45X95",145200); 
 myArray.push(prod6)
 
 
 myArray.forEach(product =>{
     console.log(product)
 });
+
+console.log(Array.isArray(myArray));
+console.log(myArray.length);
+
+//recorremos productos
+const precioDeLosProductos = myArray.map(dato=>dato.precio);
+console.log(precioDeLosProductos);
+//array de 50% de descuento
+const preciosConDescuento = myArray.map((dato)=>{
+    return{
+        nombre: dato.nombre,
+        precio:dato.precio /50
+    }
+})
+//suma total de todos los productos
+console.log(preciosConDescuento);
+
+const valorInicial=0
+
+const sumaprecios= precioDeLosProductos.reduce((acumulador , elemento)=>acumulador+elemento,valorInicial);
+console.log(`La suma total de los de los precios de todos los productos es: $${sumaprecios}`);
+
+
+
+
 
 
 /* for (let productNumber = 0; productNumber < 10; productNumber ++) {
